86fcb7c40e382afd762570719d037c3f690f2bb8
Lunerio <lunerio@outlook.es>
2020-12-27 16:30:45 +0000

    test div

--- run.c
+++ run.c
@@ -25,6 +25,7 @@ int run(char *line, stack_t **stack, unsigned int line_number)
 		{"add", add_func},
 		{"swap", swap_s},
 		{"sub", sub_f},
+		{"div", div_f},
 		{NULL, NULL}
 	};
 	line_tok = strtok(line, delim);
